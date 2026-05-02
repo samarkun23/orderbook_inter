4912c28d0efdfd082e5a0ac48be54c74f45513be
samarkun <samarkun4@gmail.com>
2026-05-02 16:02:57 +0530

    add a route create order pass the route a clone orderbook build a orderbook and write the function to create order in it

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,22 @@
+use std::sync::{Arc, Mutex};
+
 use actix_web::{App, HttpResponse, HttpServer, Responder, delete, get, post, web::{self, Json}};
 
-use crate::routes::{create_order, delete_order, get_depth};
+use crate::{orderbook::Orderbook, routes::{create_order, delete_order, get_depth}};
 
 pub mod routes;
 pub mod input;
 pub mod output;
+pub mod orderbook; 
 
 #[actix_web::main]
 async fn main() {
-    HttpServer::new(|| {
+    let orderbook = Arc::new(Mutex::new(Orderbook::new())); // this is single instance we need to give this all the endpoint 
+    // we create arc of this orderbook it's give all the thread a orderbook clone kindoff and Mutex make sure that when anythead change anything it's lock the orderbook . 
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(orderbook.clone())  // this is how you pass them all the endpoint
             .route(
                 "/",
                 web::get().to(|| async { HttpResponse::Ok().body("Hello world".to_string()) }),
--- src/routes.rs
+++ src/routes.rs
@@ -1,15 +1,21 @@
-use actix_web::{App, HttpResponse, HttpServer, Responder, delete, get, mime::JSON, post, web::{self, Json}};
+use std::sync::{Arc, Mutex};
+
+use actix_web::{App, HttpResponse, HttpServer, Responder, delete, get, mime::JSON, post, web::{self, Data, Json}};
 use serde::{Deserialize, Serialize};
 
-use crate::{input::{CreateOrderInput, DeleteOrder}, output::{CreateOrderResponse, DeleteOrderResponse, Depth}};
+use crate::{input::{CreateOrderInput, DeleteOrder}, orderbook::{self, Orderbook}, output::{CreateOrderResponse, DeleteOrderResponse, Depth}};
 
 
 #[post("/order")]
-pub async fn create_order(body: Json<CreateOrderInput>) -> impl Responder {
-    println!("{:?}", body);
-
-    // maintain orderbook logic
+pub async fn create_order(body: Json<CreateOrderInput>, orderbook: Data<Arc<Mutex<Orderbook>>>) -> impl Responder {
+    let price = body.0.price;
+    let quantity = body.0.quantity;
+    let user_id = body.0.user_id;
+    let side = body.0.side;
     
+    // maintain orderbook logic
+    let mut orderbook = orderbook.lock().unwrap(); // i was able to lock this thead over there . 
+    orderbook.create_order(price,quantity, user_id, side);
 
     HttpResponse::Ok().json(CreateOrderResponse{
         order_id: String::from("ads")
@@ -17,7 +23,7 @@ pub async fn create_order(body: Json<CreateOrderInput>) -> impl Responder {
 }
 
 #[delete("/order")]
-pub async fn delete_order(Json(body): Json<DeleteOrder>) -> impl Responder{
+pub async fn delete_order(Json(body): Json<DeleteOrder>, orderbook: Data<Orderbook>) -> impl Responder{
     let order_id = body.order_id;
 
     HttpResponse::Ok().json(DeleteOrderResponse{
@@ -27,7 +33,7 @@ pub async fn delete_order(Json(body): Json<DeleteOrder>) -> impl Responder{
 }
 
 #[get("/depth")]
-pub async fn get_depth() -> impl Responder{
+pub async fn get_depth(orderbook: Data<Orderbook>) -> impl Responder{
     HttpResponse::Ok().json(Depth {
         bids: vec![],
         asks: vec![],
